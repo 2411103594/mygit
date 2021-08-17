9ebd93b4cdb7c77b2d8733ed48996adcd992353c
liutao <2411103594@qq.com>
2021-08-17 16:55:32 +0800

    C2客户端提交

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -7,5 +7,6 @@ package org.example;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("1234564");
+        System.out.println("C2客户端1234564");
     }
 }
